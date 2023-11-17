540654f2e6a3ac319a39ca578a72840796e91897
iloozealot <iloozealot@gmail.com>
2023-11-17 15:52:53 -0600

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Methods;
+
+//for (int i = 0; i < 10; i++)
+//{
+//    SampleMethods.SayHi();
+//}
+
+//DRY - Don't Repeat Yourself
+//SOLID - SRP - Single Responsibility Principle "Something should do one thing."
+
+
+//string name = SampleMethods.GetUsersName();
+
+//SampleMethods.SayHi(name);
+
+//double result = MathShortcuts.Add(5, 3);
+//Console.WriteLine($"The result is { result }");
+
+//double[] vals = new double[] { 2, 5, 6, 21, 52, 98 };
+//MathShortcuts.AddAll(vals);
+
+//SampleMethods.SayGoodbye();
+
+// Discard Character (_)
+(string firstName, _) = SampleMethods.GetFullName();
+
+Console.WriteLine($"First Name: {firstName}");
+//Console.WriteLine($"Last Name: {lastName}");
+
+//string ageText = "33";
+//bool isValidAge = int.TryParse(ageText, out int age);
---
+++ Methods/SampleMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public static class SampleMethods
+    {
+        public static void SayHi(string firstName)
+        {
+            Console.WriteLine($"Hello {firstName}!");
+        }
+
+        public static string GetUsersName()
+        {
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        public static (string firstName, string lastName) GetFullName()
+        {
+            Console.Write("What is your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What is your last name: ");
+            string lastName = Console.ReadLine();
+
+            return (firstName,lastName);
+        }
+        public static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye User.");
+            Console.WriteLine("Thank you for visiting.");
+            Console.WriteLine("I cannot wait to see you again.");
+        }
+    }
+
+}
